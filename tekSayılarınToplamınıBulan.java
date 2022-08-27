d54fe6e526cf5288ff1d2a228f4691ddd4c9e8bd
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2022-08-27 16:40:37 +0300

    Add files via upload

---
+++ tekSayılarınToplamınıBulan.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class tekSayılarınToplamınıBulan {
+    public static void main(String[] args){
+    int n;
+    int total=0;
+    Scanner scan= new Scanner(System.in);
+
+    do{
+        System.out.println("Sayı giriniz: ");
+        n=scan.nextInt();
+        if( n % 4==0){
+            total+=n;
+        }
+
+
+    }while (n %2==0);
+        System.out.println("Toplam: "+total);
+
+
+
+
+
+    }
+}
